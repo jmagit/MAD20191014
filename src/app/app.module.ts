5fe72bf203de945a0809fba32c1822c27a1314cd
Javier <jmartin@grupoloyal.com>
2019-10-15 10:14:30 +0200

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './main';
+import { CommonAppModule } from './common-app';
+import { IndraCoreModule } from 'src/indra-core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
+    MainModule, CommonAppModule, IndraCoreModule,
     AppRoutingModule
   ],
   providers: [],

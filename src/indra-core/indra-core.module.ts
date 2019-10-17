6c222112493c93d639c0887c63efe1f3e0eff03e
Javier <jmartin@grupoloyal.com>
2019-10-17 12:24:56 +0200

    Formularios

--- src/indra-core/indra-core.module.ts
+++ src/indra-core/indra-core.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PIPES_CADENAS } from './pipes/cadenas.pipe';
 import { IndraSizerComponent } from './components/indra-sizer.component';
 import { PIPES_NUMERICOS } from './pipes/numericos.pipe';
+import { MIS_VALIDADORES } from './directives/validadores.directive';
 
 
 
 @NgModule({
   declarations: [
-    PIPES_CADENAS, IndraSizerComponent, PIPES_NUMERICOS,
+    PIPES_CADENAS, IndraSizerComponent, PIPES_NUMERICOS, MIS_VALIDADORES,
   ],
   exports: [
-    PIPES_CADENAS, IndraSizerComponent, PIPES_NUMERICOS,
+    PIPES_CADENAS, IndraSizerComponent, PIPES_NUMERICOS, MIS_VALIDADORES,
   ],
   imports: [
     CommonModule
